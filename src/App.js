2bdc5391555c63cb5abeb9fd8de10603b358533d
Rudra Sheth <your-email@example.com>
2026-03-31 16:18:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DisplayText from "./components/DisplayText";
+
+function App() {
+  return (
+    <div>
+      <h1>React Functional Component Example</h1>
+      <DisplayText />
+    </div>
+  );
+}
+
+export default App;
